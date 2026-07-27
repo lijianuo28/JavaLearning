9c0ae95038ea50cbb1f53ae64fe78d1e7ab8330d
lijianuo28 <sujfghvvhzdgnkgft@gmail.com>
2026-07-27 19:11:31 +0800

    拒单业务代码开发

--- sky-take-out/sky-server/src/main/java/com/sky/controller/admin/OrderController.java
+++ sky-take-out/sky-server/src/main/java/com/sky/controller/admin/OrderController.java
@@ -2,6 +2,7 @@ package com.sky.controller.admin;
 
 import com.sky.dto.OrdersConfirmDTO;
 import com.sky.dto.OrdersPageQueryDTO;
+import com.sky.dto.OrdersRejectionDTO;
 import com.sky.entity.Orders;
 import com.sky.result.PageResult;
 import com.sky.result.Result;
@@ -70,6 +71,18 @@ public class OrderController {
     @ApiOperation("接单")
     public Result confirm(@RequestBody OrdersConfirmDTO ordersConfirmDTO){
         orderService.confirm(ordersConfirmDTO);
+        return Result.success();
+    }
+
+    /**
+     * 拒单
+     * @param ordersRejectionDTO
+     * @return
+     */
+    @PutMapping("/rejection")
+    @ApiOperation("拒单")
+    public Result reject(@RequestBody OrdersRejectionDTO ordersRejectionDTO){
+        orderService.reject(ordersRejectionDTO);
         return Result.success();
     }
 }
